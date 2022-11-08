2c05b821618eb901bcfe85ae374c74d678bc8cdc
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2022-11-08 23:27:32 +0100

    testing if vite is fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import router from "./router/index.js";
 import ui from "./ui/index.js";
 // Import all of Bootstrap's JS
-import * as bootstrap from "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js";
+import * as bootstrap from "bootstrap";
 ui();
 router();
