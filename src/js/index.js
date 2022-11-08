702bcbb957e714ce0fc4e48a23076f01d3eb0223
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2022-11-08 23:19:30 +0100

    added . to base in vite.config.js so build will work on pages and vite

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import router from "./router/index.js";
 import ui from "./ui/index.js";
+import * as bootstrap from "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js";
 ui();
 router();
