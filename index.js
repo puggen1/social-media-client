521afe928078c3ca4c7d886c7dcdd12294374d90
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2022-11-08 23:22:44 +0100

    changing index.js bootsrap import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import router from "./router/index.js";
 import ui from "./ui/index.js";
 // Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
+import * as bootstrap from "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js";
 ui();
 router();
