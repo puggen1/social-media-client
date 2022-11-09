41e393d80b6eeb5bea2e7e7547a729d79d3053f8
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2022-11-09 12:50:10 +0100

    temp test to check github action videos

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 require("dotenv/config");
 module.exports = defineConfig({
+  projectId: "o7xdtt",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
